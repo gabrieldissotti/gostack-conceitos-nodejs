106aebe57b2f357a483f0e9700ed7fb8c44d2a9d
Gabriel Dissotti <gabrieldnrodrigues@gmail.com>
2020-04-12 16:29:13 -0300

    feat(methods): added all methods passing in tests

--- src/app.js
+++ src/app.js
@@ -1,7 +1,9 @@
 const express = require("express");
 const cors = require("cors");
 
-// const { uuid } = require("uuidv4");
+const { uuid } = require("uuidv4");
+
+const findRepository = require('./middlewares/findRepository');
 
 const app = express();
 
@@ -11,23 +13,53 @@ app.use(cors());
 const repositories = [];
 
 app.get("/repositories", (request, response) => {
-  // TODO
+  return response.json(repositories)
 });
 
 app.post("/repositories", (request, response) => {
-  // TODO
+  const data = request.body;
+
+  const repository = {
+    id: uuid(),
+    likes: 0,
+    ...data
+  }
+
+  repositories.push(repository)
+
+  return response.json(repository)
 });
 
-app.put("/repositories/:id", (request, response) => {
-  // TODO
+app.put("/repositories/:id", findRepository(repositories), (request, response) => {
+  const { 
+    url,
+    title,
+    techs
+  } = request.body;
+
+  repositories[request.repositoryIndex] = {
+    ...request.repository,
+    url,
+    title,
+    techs
+  }
+
+  return response.json(repositories[request.repositoryIndex])
 });
 
-app.delete("/repositories/:id", (request, response) => {
-  // TODO
+app.delete("/repositories/:id", findRepository(repositories), (request, response) => {
+  repositories.pop(request.repositoryIndex, 1)
+
+  return response.status(204).send()
 });
 
-app.post("/repositories/:id/like", (request, response) => {
-  // TODO
+app.post("/repositories/:id/like", findRepository(repositories), (request, response) => {
+  repositories[request.repositoryIndex] = {
+    ...request.repository,
+    likes: request.repository.likes+1
+  }
+
+  return response.json(repositories[request.repositoryIndex])
 });
 
 module.exports = app;
---
+++ src/middlewares/findRepository.js
@@ -0,0 +1,17 @@
+module.exports = function findRepository (repositories){
+  return function (request, response, next){
+  
+    const { id } = request.params;
+    
+    const index = repositories.findIndex(item => item.id === id);
+    
+    if(index < 0){
+      return response.status(400).json('Repository does not exit!')
+    }
+  
+    request.repository = repositories[index]
+    request.repositoryIndex = index;
+  
+    return next();
+  }
+}
